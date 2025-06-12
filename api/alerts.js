3cc67b9eb4d1b790e8d8f56fdeb1bb355e2f0249
phantasmat2018 <37955484+phantasmat2018@users.noreply.github.com>
2025-06-12 20:12:25 +0300

    Add files via upload

--- api/alerts.js
+++ api/alerts.js
@@ -1,19 +1,13 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  // --- ВАЖЛИВІ РЯДКИ-ДОЗВОЛИ (CORS) ---
-  // Дозволяємо запити з будь-якого джерела
+  // Дозволяємо запити з будь-якого джерела (важливо для Vercel)
   res.setHeader('Access-Control-Allow-Origin', '*');
-  // Вказуємо дозволені методи
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  // Вказуємо дозволені заголовки
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Обробка preflight-запиту, який браузер надсилає автоматично
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  // ------------------------------------
 
   const apiUrl = 'https://api.alerts.in.ua/v1/alerts/active.json';
   const apiToken = 'cce2a5a5af6ac920d797c0680135635066614ffcab2203';
